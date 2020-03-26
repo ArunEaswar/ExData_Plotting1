669446aa810f5deaba499433955f1df487588aa7
ArunEaswar <arunawerik@gmail.com>
2020-03-25 23:42:04 -0400

    plot 4 code

--- plot4.R
+++ plot4.R
@@ -1,4 +1,5 @@
 #read the file
+
 tabl<-read.table("household_power_consumption.txt", header=TRUE,sep=";", na.strings=c("?"),
                  colClasses=c("character", "character","numeric","numeric","numeric","numeric",
                               "numeric","numeric","numeric"))
